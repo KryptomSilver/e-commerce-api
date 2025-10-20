699c75eac4c49df6ded1525861cbd814e31085f7
KryptomSilver <abelromeror763@gmail.com>
2025-10-19 22:26:55 -0600

    feat(product-service): create resource product

---
+++ product-service/src/product/product.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectModel(Product.name)
+    private readonly productRepository: Model<Product>,
+  ) {}
+  create(createProductDto: CreateProductDto) {
+    const createdProduct = new this.productRepository(createProductDto);
+    return createdProduct.save();
+  }
+
+  findAll() {
+    return this.productRepository.find();
+  }
+
+  findOne(id: string) {
+    return this.productRepository.findById(id).exec();
+  }
+
+  update(id: string, updateProductDto: UpdateProductDto) {
+    return this.productRepository
+      .findByIdAndUpdate(id, updateProductDto, {
+        new: true,
+      })
+      .exec();
+  }
+
+  remove(id: number) {
+    return this.productRepository.findByIdAndDelete(id).exec();
+  }
+}
