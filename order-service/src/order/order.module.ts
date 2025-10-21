50cf612a1a9e0023ed25a49a26014cdfdb86d644
KryptomSilver <abelromeror763@gmail.com>
2025-10-21 00:15:58 -0600

    feat(order-service): implements config with environment variables and add grpc config client

--- order-service/src/order/order.module.ts
+++ order-service/src/order/order.module.ts
@@ -3,21 +3,25 @@ import { OrderService } from './order.service';
 import { OrderController } from './order.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: 'PRODUCT_PACKAGE',
-        transport: Transport.GRPC,
-        options: {
-          package: 'products',
-          protoPath: join(__dirname, '../../../proto/products.proto'),
-          loader: {
-            includeDirs: [join(__dirname, '../../../proto')],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.GRPC,
+          options: {
+            package: 'products',
+            protoPath: join(__dirname, '../../proto/products.proto'),
+            loader: {
+              includeDirs: [join(__dirname, '../proto')],
+            },
+            url: configService.get<string>('URL_GRPC_SERVER'),
           },
-          url: 'localhost:5051',
-        },
+        }),
       },
     ]),
   ],
