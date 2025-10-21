d432767645bd9a3388c8ecbd016ad8f496d8a0ad
KryptomSilver <abelromeror763@gmail.com>
2025-10-21 00:12:56 -0600

    feat(order-service): implements config for environment variables

--- order-service/src/app.module.ts
+++ order-service/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderModule } from './order/order.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [OrderModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
