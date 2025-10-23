4ca644dd94b01af94b3ffd20300812389aa27f5c
KryptomSilver <abelromeror763@gmail.com>
2025-10-22 18:13:54 -0600

    feat(notification-service): implements connection for rabbitmq

--- notification-service/src/main.ts
+++ notification-service/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+  logger.log('Starting RabbitMQ microservice...');
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
+      queue: process.env.RABBITMQ_QUEUE || 'notification_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });
+  await app.startAllMicroservices();
+  logger.log('Microservice is listening on RabbitMQ');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
