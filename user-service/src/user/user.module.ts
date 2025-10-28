c5dd7894af71c52bc6cb382960123647805e5b0e
KryptomSilver <abelromeror763@gmail.com>
2025-10-28 14:00:07 -0600

    feat(user-service): Implement RabbitMQ producer for user.created event
    Connects the user-service to RabbitMQ to publish an event whenever a new user is created. This allows other services (like the notification-service) to react to new user registrations.
    
    - Adds `amqplib` and `amqp-connection-manager` dependencies.
    - Imports and configures `ClientsModule` in `UserModule` to register the `NOTIFICATION_SERVICE` RabbitMQ client.
    - Injects the `ClientProxy` into `UserService`.
    - Emits a `user.created` event in the `create` method, sending the new user's details to the message broker.

--- user-service/src/user/user.module.ts
+++ user-service/src/user/user.module.ts
@@ -3,9 +3,29 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ClientsModule.registerAsync([
+      {
+        name: 'NOTIFICATION_SERVICE',
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [`${configService.get<string>('RABBITMQ_URL')}`],
+            queue: 'notification_queue',
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+      },
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
--- user-service/src/user/user.service.ts
+++ user-service/src/user/user.service.ts
@@ -1,17 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
+    @Inject('NOTIFICATION_SERVICE')
+    private readonly notificationService: ClientProxy,
   ) {}
   create(createUserDto: CreateUserDto) {
     const userValue = this.userRepository.create(createUserDto);
+    this.notificationService.emit('user.created', {
+      id: userValue.id,
+      name: userValue.name,
+      email: userValue.email,
+    });
     return this.userRepository.save(userValue);
   }
 
