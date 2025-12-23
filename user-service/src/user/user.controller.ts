869617e12d4553ba90ed55df3ab3ab98790f1ade
KryptomSilver <abelromeror763@gmail.com>
2025-12-23 10:51:02 -0600

    feat(user-service): add routes names and add async/await

--- user-service/src/user/user.controller.ts
+++ user-service/src/user/user.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -12,23 +11,13 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
-  @Get()
-  findAll() {
-    return this.userService.findAll();
+  @Post('login')
+  async login(@Body('email') email: string, @Body('password') password: string) {
+    return await this.userService.login(email, password);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  @Get('profile')
+  async getProfile(@Body('token') token: string) {
+    return await this.userService.getProfile(token);
   }
 }
