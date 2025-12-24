4bba74d3857661fc300bd660e616a94855cedf02
KryptomSilver <abelromeror763@gmail.com>
2025-12-24 14:36:54 -0600

    feat(product-service): add interfaces for grpc

--- product-service/src/product/interfaces/product-service-grpc.interface.ts
+++ product-service/src/product/interfaces/product-service-grpc.interface.ts
@@ -1,3 +1,17 @@
+export interface Product{
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  category: string;
+}
 export interface FindOneRequest {
   id: string;
+}
+export interface FindRequest {
+  ids: string[];
+}
+export interface ProductList {
+  products: Product[];
 }
