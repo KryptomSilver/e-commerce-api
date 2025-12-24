7c0ca5c4efd48220dfaf2cb06d134bee47cecb65
KryptomSilver <abelromeror763@gmail.com>
2025-12-24 11:53:58 -0600

    feat(order-service): add interface and import grpc for product service
    In this commit, I have added the import from service with grpc (product-service), and create the interface from product-service. In addition validate the productId with the product-service (grpc) from function findByIds and add transaction database.

--- order-service/src/order/interfaces/product-service.interface.ts
+++ order-service/src/order/interfaces/product-service.interface.ts
@@ -1,15 +1,23 @@
-export interface ProductsService {
-  findOne(data: FindOneRequest): Product;
-}
+import { Observable } from 'rxjs';
 
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  stock: number;
+  category: string;
+}
 export interface FindOneRequest {
   id: string;
 }
-
-export interface Product {
-  _id: string;
-  name: string;
-  price: number;
-  category: string;
-  description: string;
+export interface FindManyRequest {
+  ids: string[];
+}
+export interface ProductList {
+  products: Product[];
+}
+export interface ProductsGrpcService {
+  findOne(data: { id: string }): Observable<Product>;
+  findByIds(data: FindManyRequest): Observable<ProductList>;
 }
--- order-service/src/order/order.service.ts
+++ order-service/src/order/order.service.ts
@@ -1,29 +1,86 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Inject,
+  Injectable,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
-import { InjectRepository } from '@nestjs/typeorm';
 import { Order } from './entities/order.entity';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { OrderItem } from './entities/order-item.entity';
+import { ClientGrpc } from '@nestjs/microservices';
+import { InjectRepository } from '@nestjs/typeorm';
+import { lastValueFrom } from 'rxjs';
+import { ProductsGrpcService } from './interfaces/product-service.interface';
 
 @Injectable()
-export class OrderService {
+export class OrderService implements OnModuleInit {
+  private productServiceGrpc: ProductsGrpcService;
   constructor(
     @InjectRepository(Order)
     private readonly orderRepository: Repository<Order>,
+    @Inject('PRODUCT_PACKAGE')
+    private readonly productService: ClientGrpc,
+    private dataSource: DataSource,
   ) {}
-
-  async create(createOrderDto: CreateOrderDto) {
-    const orderValue = this.orderRepository.create(createOrderDto);
-    const newOrder = await this.orderRepository.save(orderValue);
-    const response = {
-      data: newOrder,
-      message: 'Order created successfully',
-    };
-    return response;
+  onModuleInit() {
+    this.productServiceGrpc =
+      this.productService.getService<ProductsGrpcService>('ProductsService');
   }
 
-  findAll() {
-    return `This action returns all order`;
+  async create(createOrderDto: CreateOrderDto) {
+    const productIds = createOrderDto.orderItems.map(
+      (orderItem) => orderItem.productId,
+    );
+    const responseProducts = await lastValueFrom(
+      this.productServiceGrpc.findByIds({ ids: productIds }),
+    );
+    const products = responseProducts?.products ?? [];
+    return await this.dataSource.transaction(async (manager) => {
+      const newOrder = new Order();
+      newOrder.orderNumber = createOrderDto.orderNumber;
+      newOrder.createdAt = createOrderDto.createdAt;
+      newOrder.userId = createOrderDto.userId;
+      newOrder.status = createOrderDto.status;
+
+      if (products.length !== productIds.length) {
+        throw new NotFoundException('One or more products not found');
+      }
+
+      let totalAmount = 0;
+      newOrder.orderItems = createOrderDto.orderItems.map((item) => {
+        const orderItem = new OrderItem();
+        const product = products.find((prod) => prod.id === item.productId);
+        console.log(product, item.productId, products);
+        if (!product) {
+          throw new NotFoundException(
+            `Product with ID ${item.productId} not found`,
+          );
+        }
+        orderItem.productId = item.productId;
+        orderItem.quantity = product.stock >= item.quantity ? item.quantity : 0;
+        orderItem.price = product.price;
+        totalAmount += product.price * item.quantity;
+        return orderItem;
+      });
+      newOrder.totalAmount = totalAmount;
+      const savedOrder = await manager.save(Order, newOrder);
+      const response = {
+        data: savedOrder,
+        message: 'Order created successfully',
+      };
+      return response;
+    });
+  }
+
+  async findAll() {
+    const orders = await this.orderRepository.find({
+      relations: {
+        orderItems: true,
+      },
+    });
+    return orders;
   }
 
   findOne(id: number) {
