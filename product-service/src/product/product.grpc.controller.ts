bfc16b6b01437952e24820037753305b39fff313
KryptomSilver <abelromeror763@gmail.com>
2026-01-31 20:41:32 -0600

    feat(product-service): Add method for controller in grpc call function decrease stock

--- product-service/src/product/product.grpc.controller.ts
+++ product-service/src/product/product.grpc.controller.ts
@@ -2,6 +2,7 @@ import { Controller } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { GrpcMethod } from '@nestjs/microservices';
 import {
+  DecreaseStockRequest,
   FindOneRequest,
   FindRequest,
 } from './interfaces/product-service-grpc.interface';
@@ -20,11 +21,27 @@ export class ProductGrpcController {
     const formattedProducts = items.map((item) => ({
       id: item._id.toString(),
       name: item.name || '',
-      price: Math.round(item.price || 0),
+      price: item.price || 0,
       category: item.category || '',
       description: item.description || '',
       stock: String(item.stock || '0'),
     }));
     return { products: formattedProducts };
+  }
+  @GrpcMethod('ProductsService', 'DecreaseStock')
+  async decreaseStock(request: DecreaseStockRequest) {
+    const item = await this.productService.decreaseStock(
+      request.id,
+      request.quantity,
+    );
+    const product = {
+      id: item._id.toString(),
+      name: item.name || '',
+      price: item.price || 0,
+      category: item.category || '',
+      description: item.description || '',
+      stock: String(item.stock || '0'),
+    };
+    return product;
   }
 }
